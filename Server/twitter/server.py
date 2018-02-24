f1833c021c68a2d1a22b1266e92feda20066efd3
prahladanand17 <anprahlad@gmail.com>
2018-02-24 02:54:36 -0600

    1st commit

--- Server/twitter/server.py
+++ Server/twitter/server.py
@@ -5,7 +5,7 @@ from Server.twitter.routes.root import loginBluePrint
 from Server.twitter.routes.home import homeBlueprint
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='style')
+    app = Flask(__name__, template_folder='../templates', static_folder='../Client/style')
     app.register_blueprint(loginBluePrint)
     app.register_blueprint(homeBlueprint)
     return app
